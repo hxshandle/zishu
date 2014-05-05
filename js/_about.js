5f4a28586f7dabc9c52048b99dd645ca722fade6
hxshandle <handle.huang0409@gmail.com>
2014-05-05 22:31:46 +0800

    bb

--- js/_about.js
+++ js/_about.js
@@ -1,4 +1,14 @@
 $(function(){
   scrollable('.scrollable.active');
 
+  $('#about .item a').click(function(){
+    $('#about .item.active').removeClass('active');
+    var $this = $(this);
+    var ref = $this.data('ref');
+    $this.parent().addClass('active');
+    $('.details-outer .active').removeClass('active').fadeOut();
+    $('#'+ref).fadeIn().addClass('active').jScrollPane();
+    
+  });
+
 });
