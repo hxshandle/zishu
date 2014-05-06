9afd46e3276bbac61d31506fabf0b3f8fc1e82d1
Handle (Xiaosheng) Huang <hhuang@successfactors.com>
2014-05-06 20:15:26 +0800

    ab

--- js/_common.js
+++ js/_common.js
@@ -1,3 +1,15 @@
 function scrollable(selector){
   $(selector).jScrollPane();
 }
+
+function switchContent(toggleSelector,relatedSelector){
+  $(toggleSelector).click(function(){
+    $(toggleSelector).filter('.active').removeClass('active');
+    var $this = $(this);
+    var ref = $this.data('ref');
+    $this.addClass('active');
+    $(relatedSelector).filter('.active').removeClass('active').css({display:'none'});
+    $('#'+ref).fadeIn().addClass('active').jScrollPane();
+  });
+
+}
